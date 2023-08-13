23f41c056c567ebac79d53fe1568176422b44a8c
Allan <76095439+ZenCrash@users.noreply.github.com>
2023-08-13 16:43:13 +0200

    initial project

---
+++ GameResourcePlanner/Data/Recepie.cs
@@ -0,0 +1,13 @@
+﻿namespace GameResourcePlanner.Data
+{
+    public class Recepie
+    {
+        public string MachineName { get; set; }
+        public Dictionary<string, int> Inputs { get; set; }
+        public Dictionary<string, int> Outputs { get; set; }
+        public double TotalEU { get; set; }
+        public double EUPerTick { get; set; }
+        public double CraftingTime { get; set; }
+    }
+}
+
---
+++ GameResourcePlanner/Data/RecepieLogic.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+
+namespace GameResourcePlanner.Data
+{
+    public class RecepieLogic
+    {
+        private string filePath = @"..\GameResourcePlanner\GameResourcePlanner\Data\DataStorage\Recepies.json"; // Update this with your file path
+
+        //GetAll Recepies
+        public List<Recepie> GetRecepiesFromJson()
+        {
+            if (File.Exists(filePath))
+            {
+                string jsonData = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<List<Recepie>>(jsonData);
+            }
+            else
+            {
+                return new List<Recepie>();
+            }
+        }
+
+        //Insert List<Recepie>
+        public void InsertRecepiesToJson(List<Recepie> recepies)
+        {
+            string jsonData = JsonSerializer.Serialize(recepies, new JsonSerializerOptions { WriteIndented = true });
+
+            File.WriteAllText(filePath, jsonData);
+        }
+
+        //Insert Recepie
+        public void InsertRecepie(Recepie newRecepie)
+        {
+            List<Recepie> existingRecepies = GetRecepiesFromJson();
+            List<Recepie> UpdatedRecepies = new();
+
+            //if (existingRecepies.Exists(r => r.MachineName == newRecepie.MachineName &&
+            //                                 r.Inputs.Count == newRecepie.Inputs.Count &&
+            //                                 r.Inputs.Values.All(value => newRecepie.Inputs.ContainsValue(value)) &&
+            //                                 r.Outputs.Count == newRecepie.Outputs.Count &&
+            //                                 r.Outputs.Values.All(value => newRecepie.Outputs.ContainsValue(value)) &&
+            //                                 r.TotalEU == newRecepie.TotalEU &&
+            //                                 r.EUPerTick == newRecepie.EUPerTick &&
+            //                                 r.CraftingTime == newRecepie.CraftingTime
+            //                                 ))
+            //{
+            //    throw new Exception($"Recipe already exists.");
+            //}
+
+            UpdatedRecepies = existingRecepies;
+            UpdatedRecepies.Add(newRecepie);
+
+            string jsonData = JsonSerializer.Serialize(UpdatedRecepies, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, jsonData);
+        }
+    }
+}
